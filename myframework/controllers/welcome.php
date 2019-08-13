846592bcefa8a8f31ef5b16c9026652cc34def93
MRodriguez <msrodriguez@fullsail.edu>
2019-08-12 20:15:06 -0400

    ### Welcome Page
    - Cleans up the welcome page.
    - Adds body content to the welcome page.
    - Cleans up the welcome page styling.

--- myframework/controllers/welcome.php
+++ myframework/controllers/welcome.php
@@ -9,19 +9,13 @@ class welcome extends AppController{
 
   public function home(){
     $data = array();
-    $data["pagename"] = "home";
-    $data["navigation"] = array("home"=>"/home","brews"=>"/brews","about"=>"/about");
+    $data["pagename"] = "welcome";
+    $data["navigation"] = array("welcome"=>"/welcome","brews"=>"/brews","about"=>"/about");
 
     $this->parent->getView("header", $data);
-    $this->parent->getView("body");
+    $this->parent->getView("welcome", $data);
     $this->parent->getView("footer");
   }
-
-  public function about(){
-  }
-
 }
-
-
 
  ?>
---
+++ myframework/views/welcome.php
@@ -0,0 +1,11 @@
+<section class='center'
+<?php
+  echo 'id="'.$data["pagename"].'"';
+?>
+>
+
+  <img src='./assets/cauldron.svg' alt='bubbling cauldron'/>
+
+  <p>Here at <b>Witches' Brew</b>, we pride ourselves in delivering the most potent flasks and potions.  Our products are all made from natural ingredients and stirred by hand.</p>
+
+</section>
