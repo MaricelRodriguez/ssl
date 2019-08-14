37ab230f15a1a5d5db0d86a4108ee6894bd38dd8
MRodriguez <msrodriguez@fullsail.edu>
2019-08-14 14:58:41 -0400

    ### Assignment 3
    - Adds progress bar to the brews page.
    - Adds headings to each brew.
    - Adds a popover to the about page.

--- myframework/views/about.php
+++ myframework/views/about.php
@@ -8,4 +8,9 @@
 <p>The <b>Witches' Brew</b> shop has been open since 1693, however, no records indicate the origin of the store.  It's almost as if it appeared out of thin air.</p>
 <p></p>
 
+<button type="button" class="" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
+sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on bottom
+</button>
+
 </section>
--- myframework/views/brews.php
+++ myframework/views/brews.php
@@ -8,13 +8,25 @@
   foreach($data["brews"] as $key=>$description){
     echo '<div class="brew row">';
     echo '<img class="col-md" src="./assets/images/'.str_replace(' ', '', $key).'.svg" alt="'.$key.'"/>';
-    echo '<p class="col-md-6">'.$description.'</p>';
+    echo '<div class="col-md-6">';
+    echo '<h4>'.$key.'</h3>';
+    echo '<p>'.$description.'</p>';
+    echo '</div>';
     echo '<div class="col-md btn-wrap text-center">';
     echo '<button class="" type="button" data-toggle="modal" data-target="#purchaseBrew">Purchase</button>';
     echo '</div>';
     echo '</div>';
   }
 ?>
+
+
+<div class="progress-wrap">
+  <h4>Our Next Brew is Coming</h4>
+  <p>Stay on the lookout for our upcoming brew.  It's in the cauldron!</p>
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+  </div>
+</div>
 
 <div class="modal fade" id="purchaseBrew" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
