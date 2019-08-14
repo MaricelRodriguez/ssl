abbbaee6d853666444827712a04dd50b418e18ee
MRodriguez <msrodriguez@fullsail.edu>
2019-08-14 14:11:20 -0400

    ### Assignment 3
    - Adds purchase modal to the brews page.
    - Adds buttons to each brew to open modal.

--- myframework/views/brews.php
+++ myframework/views/brews.php
@@ -1,4 +1,4 @@
-<section class='center'
+<section class='center container'
 <?php
   echo 'id="'.$data["pagename"].'"';
 ?>
@@ -6,11 +6,27 @@
 
 <?php
   foreach($data["brews"] as $key=>$description){
-    echo '<div class="brew">';
-    echo '<img src="./assets/images/'.str_replace(' ', '', $key).'.svg" alt="'.$key.'"/>';
-    echo '<p>'.$description.'</p>';
+    echo '<div class="brew row">';
+    echo '<img class="col-md" src="./assets/images/'.str_replace(' ', '', $key).'.svg" alt="'.$key.'"/>';
+    echo '<p class="col-md-6">'.$description.'</p>';
+    echo '<div class="col-md btn-wrap text-center">';
+    echo '<button class="" type="button" data-toggle="modal" data-target="#purchaseBrew">Purchase</button>';
+    echo '</div>';
     echo '</div>';
   }
 ?>
+
+<div class="modal fade" id="purchaseBrew" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <p>Your purchase is mysteriously on its way. Our bat will collect the funds upon arrival.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 </section>
