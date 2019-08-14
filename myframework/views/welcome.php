bf3738acb03ca8bb4a6922b92dbc061983d59a55
MRodriguez <msrodriguez@fullsail.edu>
2019-08-14 14:30:08 -0400

    ### Assignment 3
    - Adds carousel to the welcome page.

--- myframework/views/welcome.php
+++ myframework/views/welcome.php
@@ -3,8 +3,29 @@
   echo 'id="'.$data["pagename"].'"';
 ?>
 >
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./assets/images/carousel1.jpg" class="img-fluid w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./assets/images/carousel2.jpg" class="img-fluid w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./assets/images/carousel3.jpg" class="img-fluid w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
-  <img src='./assets/images/cauldron.svg' alt='bubbling cauldron'/>
+  <img class='page-img' src='./assets/images/cauldron.svg' alt='bubbling cauldron'/>
 
   <p>Here at <b>Witches' Brew</b>, we pride ourselves in delivering the most potent flasks and potions.  Our products are all made from natural ingredients and stirred by hand.</p>
 
